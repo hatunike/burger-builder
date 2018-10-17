e18ab13a0241d7e94575c80308f785f9cda221f2
Charles Hart <charles@busybusy.com>
2018-10-17 10:14:40 -0600

    Add more and less buttons and order button with some logic

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,8 +1,28 @@
 import React from 'react';
 
-const buildControl = (props) => (
-    <div></div>
+import classes from './BuildControls.module.css'
+import BuildControl from './BuildControl/BuildControl'
+
+const controls = [
+    { label: 'Salad', type: 'salad'},
+    { label: 'Bacon', type: 'bacon'},
+    { label: 'Cheese', type: 'cheese'},
+    { label: 'Meat', type: 'meat'},
+];
+
+const buildControls = (props) => (
+    <div className={classes.BuildControls}>
+        <p>Current price : <strong>{props.currentPrice.toFixed(2)}</strong></p>
+        {controls.map(ctrl => (
+            <BuildControl key={ctrl.label} 
+                          label={ctrl.label} 
+                          added={() => props.ingredientAdded(ctrl.type)} 
+                          removed= {() => props.ingredientRemoved(ctrl.type)}
+                          disabled= {props.disabled[ctrl.type]}/>
+        ))}
+        <button className={classes.OrderButton} disabled={!props.purcheasable}>ORDER NOW</button>
+    </div>
 );
  
 
-export default buildControl;
+export default buildControls;
