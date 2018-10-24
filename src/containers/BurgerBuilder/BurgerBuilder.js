5e7e5639f1c2d2c1895bbd2dbf5ce10a81529661
Charles Hart <charles@busybusy.com>
2018-10-24 14:39:02 -0600

    add backdrop, add buttons to order summary. Add Custom Buttons

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -70,6 +70,14 @@ class BurgerBuilder extends Component {
 		this.setState({purchasing:true})
 	}
 
+	purchaseCancelHandler = () => {
+		this.setState({purchasing:false})
+	}
+
+	purchaseContinueHandler = () => {
+		alert('You continue');
+	}
+
 	render () {
 		const disabledInfo = {
 			...this.state.ingredients
@@ -82,8 +90,13 @@ class BurgerBuilder extends Component {
 			
 			<Aux>
 
-				<Modal show={this.state.purchasing}>
-					<OrderSummary ingredients={this.state.ingredients}/>
+				<Modal show={this.state.purchasing} backdropClick= {this.purchaseCancelHandler}>
+					<OrderSummary 
+					price={this.state.totalPrice}
+					ingredients={this.state.ingredients} 
+					purchaseCanceled={this.purchaseCancelHandler}
+					purchaseContinued={this.purchaseContinueHandler} />
+					
 				</Modal>
 				<Burger ingredients={this.state.ingredients}/>
 				<BuildControls  ingredientAdded={this.addIngredientHandler} 
