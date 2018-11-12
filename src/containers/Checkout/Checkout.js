c3a0b218596d6d7e9be1e44fc57491268cf6c2f3
Charles Hart <charles@busybusy.com>
2018-11-12 13:35:05 -0700

    Add basic checkout, currently showing without routing

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
+import CheckoutSummary from '../../components/Layout/UI/Order/CheckoutSummary/CheckoutSummary';
 
 class Checkout extends Component {
-  state = {  }
+  state = { 
+      ingredients: {
+        salad: 1,
+        meat: 1,
+        cheese: 1,
+        bacon: 1
+      }
+   }
   render() { 
     return (<div>
-
+      <CheckoutSummary ingredients={this.state.ingredients}/>
     </div>);
   }
 }
